e51c000a0d8d5d9da1110a59b52cc388b50c47ac
Aubrey H. Laverty <ahlaverty@aubreyhlaverty.io>
2020-07-07 16:19:25 -0500

    Fix all breakpoints

--- src/gradient/Gradient.js
+++ src/gradient/Gradient.js
@@ -5,7 +5,6 @@ import './gradient.css';
 export default function Gradient () {
     return (
         <div>
-            {/* <div className="gradient-yellow-top"></div> */}
             <div className="gradient-green-top"></div>
             <div className="gradient-bottom"></div>
         </div>
