720b43c9dc1838c4d6d76b41d3ea7302673067fc
Aubrey H. Laverty <ahlaverty@aubreyhlaverty.io>
2023-06-14 14:41:11 -0500

    fix portfolio, delete portfolio copy

--- src/website-components/portfolio/PortfolioContent.js
+++ src/website-components/portfolio/PortfolioContent.js
@@ -24,7 +24,7 @@ export default function PortfolioContent() {
                      <Card.Body>
                         <Card.Title>Transitioning off WordPress</Card.Title>
                         <Card.Text>When I was at Investor Cash Management, their corporate site was setup on WordPress. No one had previously actively maintained it, so the site was crashing pretty regularly.</Card.Text>
-                        <Card.Text>As a stop gap until the new website design was approved, I offered to redo the website and move it onto an Amazon S3 bucket. The site was successfully transistioned. It can be seen at <a href="https:///www.investorcashmanagement.com">www.investorcashmanagement.com</a>.</Card.Text>
+                        <Card.Text>As a stop gap until the new website design was approved, I offered to redo the website and move it onto an Amazon S3 bucket. The site was successfully transistioned.</Card.Text>
                      </Card.Body>
                      <Card.Footer>
                         <Card.Text><span className="font-bold">Technologies used:</span> HTML, CSS, SCSS, Bootstrap 4</Card.Text>
@@ -54,31 +54,6 @@ export default function PortfolioContent() {
                   </Card> */}
                </CardDeck>
             </Col>
-			</Row>
-			<Row  className="mt-5 text-black">
-				<Col lg="auto">
-					<Card>
-						<Card.Body>
-							<Card.Title>Design Examples</Card.Title>
-							<Row>
-								<Col>
-									<h6>Debit Card Examples</h6>
-								</Col>
-							</Row>
-							<Row>
-								<Col>
-									<Image fluid className="design-example-image" src={designExample1} />
-								</Col>
-								<Col>
-									<Image fluid className="design-example-image" src={designExample2} />
-								</Col>
-								<Col>
-									<Image fluid className="design-example-image" src={designExample3} />
-								</Col>
-							</Row>
-						</Card.Body>
-					</Card>
-				</Col>	
 			</Row>
       </Col>
 
