d34fa5deb45ad691c1796a261199cbd33b1f5bed
Aubrey H. Laverty <ahlaverty@aubreyhlaverty.io>
2023-12-22 11:23:51 -0600

    Remove to do list

--- src/website-components/portfolio/Portfolio.js
+++ src/website-components/portfolio/Portfolio.js
@@ -1,29 +1,27 @@
-import React from 'react';
-import { Route, Switch, useRouteMatch, BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { Route, Switch, useRouteMatch, BrowserRouter } from "react-router-dom";
 
-import PortfolioContent from './PortfolioContent';
-import ToDoList from '../../todo-list/ToDoList';
-import TaskDashboard from '../../task-dashboard/Dashboard';
+import PortfolioContent from "./PortfolioContent";
+import ToDoList from "../../todo-list/ToDoList";
+import TaskDashboard from "../../task-dashboard/Dashboard";
 
-
-import './portfolio.css';
+import "./portfolio.css";
 
 export default function Porfolio() {
-	let { path } = useRouteMatch();
-	return(
-		<BrowserRouter>
-			<Switch>
-				<Route exact path="/portfolio">
-					<PortfolioContent />
-				</Route>
-				<Route exact path={`${path}/todo-list`}>
+  let { path } = useRouteMatch();
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/portfolio">
+          <PortfolioContent />
+        </Route>
+        {/* <Route exact path={`${path}/todo-list`}>
 					<ToDoList/>
-				</Route>
-				<Route exact path={`${path}/task-dashboard`}>
-					<TaskDashboard />
-				</Route>
-			</Switch>
-		</BrowserRouter>
-
-	);
+				</Route> */}
+        <Route exact path={`${path}/task-dashboard`}>
+          <TaskDashboard />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
--- src/website-components/portfolio/PortfolioContent.js
+++ src/website-components/portfolio/PortfolioContent.js
@@ -1,32 +1,42 @@
-import React from 'react';
-import { Row, Col, CardDeck, Card } from 'react-bootstrap';
-import { Link, useRouteMatch } from 'react-router-dom';    
-         
+import React from "react";
+import { Row, Col, CardDeck, Card } from "react-bootstrap";
+import { Link, useRouteMatch } from "react-router-dom";
+
 export default function PortfolioContent() {
+  let { path } = useRouteMatch();
 
-   let { path } = useRouteMatch();
-
-   return (
-      <Col>
-         <Row>
-            <Col>
-               <h1>Projects</h1>
-            </Col>
-         </Row>
-         <Row>
-				<Col>
-               <CardDeck>
-                  <Card>
-                     <Card.Body>
-                        <Card.Title>Transitioning off WordPress</Card.Title>
-                        <Card.Text>When I was at Investor Cash Management, their corporate site was setup on WordPress. No one had previously actively maintained it, so the site was crashing pretty regularly.</Card.Text>
-                        <Card.Text>As a stop gap until the new website design was approved, I offered to redo the website and move it onto an Amazon S3 bucket. The site was successfully transistioned.</Card.Text>
-                     </Card.Body>
-                     <Card.Footer>
-                        <Card.Text><span className="font-bold">Technologies used:</span> HTML, CSS, SCSS, Bootstrap 4</Card.Text>
-                     </Card.Footer>
-                  </Card>
-                  <Card>
+  return (
+    <Col>
+      <Row>
+        <Col>
+          <h1>Projects</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <CardDeck>
+            <Card>
+              <Card.Body>
+                <Card.Title>Transitioning off WordPress</Card.Title>
+                <Card.Text>
+                  When I was at Investor Cash Management, their corporate site
+                  was setup on WordPress. No one had previously actively
+                  maintained it, so the site was crashing pretty regularly.
+                </Card.Text>
+                <Card.Text>
+                  As a stop gap until the new website design was approved, I
+                  offered to redo the website and move it onto an Amazon S3
+                  bucket. The site was successfully transistioned.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <Card.Text>
+                  <span className="font-bold">Technologies used:</span> HTML,
+                  CSS, SCSS, Bootstrap 4
+                </Card.Text>
+              </Card.Footer>
+            </Card>
+            {/* <Card>
                      <Card.Body>
                         <Card.Title>To-do List ReactJS App</Card.Title>
                         <Card.Text>
@@ -37,8 +47,8 @@ export default function PortfolioContent() {
                      <Card.Footer>
                         <Card.Text><span className="font-bold">Technologies used:</span> ReactJS, SCSS, React-Bootstrap</Card.Text>
                      </Card.Footer>
-                  </Card>
-                  {/* <Card>
+                  </Card> */}
+            {/* <Card>
                      <Card.Body>
                         <Card.Title>Task Dashboard</Card.Title>
                         <Card.Text>This is a task dashboard app. It allows you to see the status of the items, add items, and complete them.</Card.Text>
@@ -48,15 +58,9 @@ export default function PortfolioContent() {
                         <Card.Text><span className="font-bold">Technologies used:</span> ReactJS, SCSS, React-Bootstrap</Card.Text>
                      </Card.Footer>
                   </Card> */}
-               </CardDeck>
-            </Col>
-			</Row>
-      </Col>
-
-   );
+          </CardDeck>
+        </Col>
+      </Row>
+    </Col>
+  );
 }
-         
-
-
-
-            
