d8a6ea9b5f48ea35fa42cafc9d868fed75551750
Aubrey Laverty <ahlaverty@aubreyhlaverty.io>
2025-03-01 08:17:14 -0600

    update about pg
    remove dup developer, change color of mercy home.

--- src/website-components/about/About.js
+++ src/website-components/about/About.js
@@ -8,13 +8,13 @@ export default function About() {
         <Col>
             <Row>
                 <Col>
-                    <h1 className="about">I am <span className="name">Aubrey H. Laverty</span>, a <span className="role-1">SharePoint Developer</span> + <span className="role-2">Power Platform Developer</span>.</h1>
+                    <h1 className="about">I am <span className="name">Aubrey H. Laverty</span>, a <span className="role-1">SharePoint</span> + <span className="role-2">Power Platform Developer</span>.</h1>
                 </Col>
             </Row>
             <Row>
                 <Col>
                     <h2 class="about">In addition to being a developer, I enjoy making art and volunteering in my spare time.</h2>
-                    <h2 class="about">I sit on the Associate Board at <a href="https://www.mercyhome.org">Mercy Home for Boys and Girls</a> based in Chicago, IL. I have been volunteering with the board for several years.</h2>
+                    <h2 class="about">I sit on the Associate Board at <a href="https://www.mercyhome.org" class="role-2">Mercy Home for Boys and Girls</a> based in Chicago, IL. I have been volunteering with the board for several years.</h2>
                 </Col>
             </Row>
         </Col>
